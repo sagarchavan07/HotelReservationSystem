b0278caa92a2907162aa0552523742efdd992f2f
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-11 09:48:19 +0530

    add weekday and weekend rates in hotel

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -8,21 +8,26 @@ public class HotelReservationTest {
     @BeforeAll
     static void initialize() {
         hotelReservationService = new HotelReservationService();
-        hotelReservationService.addHotel(new Hotel("Lakewood", 110));
-        hotelReservationService.addHotel(new Hotel("Bridgewood", 160));
-        hotelReservationService.addHotel(new Hotel("Ridgewood", 220));
+        hotelReservationService.addHotel(new Hotel("Lakewood", 110, 90));
+        hotelReservationService.addHotel(new Hotel("Bridgewood", 160, 60));
+        hotelReservationService.addHotel(new Hotel("Ridgewood", 220, 150));
     }
 
     @Test
     void givenHotel_ShouldBe_AddedToHotelList() {
-        boolean result = hotelReservationService.addHotel(new Hotel("Lakewood", 110));
+        boolean result = hotelReservationService.addHotel(new Hotel("Lakewood", 110, 90));
         Assertions.assertTrue(result);
     }
 
     @Test
-    void givenCheckInAndCheckOutDates_userShould_GetCheapestHotel() {
+    void givenCheckInAndCheckOutDates_ShouldReturn_CheapestHotel() {
         Hotel hotel = hotelReservationService.getCheapestHotel("01-08-2022", "10-08-2022");
         Assertions.assertEquals("Lakewood", hotel.getName());
     }
 
+    @Test
+    void givenWeekendDates_ShouldReturn_CheapestHotel() {
+        Hotel hotel = hotelReservationService.getCheapestHotel("13-08-2022", "14-08-2022");
+        Assertions.assertEquals("Bridgewood", hotel.getName());
+    }
 }
