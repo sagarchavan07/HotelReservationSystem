a4864a57122a0db3961cbe815a386e619bc26286
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-11 19:37:45 +0530

    used streams, Exceptions and regex for input date validation

--- src/main/java/HotelReservationMain.java
+++ src/main/java/HotelReservationMain.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class HotelReservationMain {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws DateFormatException {
         System.out.println("Welcome to Hotel Reservation Program");
+        Scanner scanner = new Scanner(System.in);
         HotelReservationService hotelReservationService = new HotelReservationService();
+
         hotelReservationService.addHotel(new Hotel("Lakewood", 110, 80, 90, 80, 3));
         hotelReservationService.addHotel(new Hotel("Bridgewood", 160, 110, 60, 50, 4));
         hotelReservationService.addHotel(new Hotel("Ridgewood", 220, 100, 150, 40, 5));
 
-        System.out.println("Enter check-in date: (dd-mm=yyyy) ex. 01-01-2000");
+        System.out.println("Enter check-in date: (dd-mm-yyyy OR dd/mm/yyyy) ");
         String checkInDate = scanner.nextLine();
-        System.out.println("Enter check-out date: (dd-mm=yyyy)");
+        System.out.println("Enter check-out date: (dd-mm-yyyy)");
         String checkOutDate = scanner.nextLine();
-        Hotel cheapestHotel = hotelReservationService.getCheapestBestRatedHotel(checkInDate, checkOutDate, true);
-        System.out.println("Cheapest Hotel: " + cheapestHotel.getName() + ", Ratings : " + cheapestHotel.getRatings() + ", Total Cost: " + cheapestHotel.getTotalCost());
+
+        Hotel cheapestHotel = hotelReservationService.getCheapestBestRatedHotel(checkInDate, checkOutDate, HotelReservationService.CustomerType.REWARDED);
+        System.out.println("Cheapest Hotel for Rewarded customer: " + cheapestHotel.getName() + ", Ratings : " + cheapestHotel.getRating() + ", Total Cost: " + cheapestHotel.getTotalCost());
+
     }
 }
--- src/main/java/HotelReservationService.java
+++ src/main/java/HotelReservationService.java
@@ -1,10 +1,15 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class HotelReservationService {
+    public enum CustomerType {
+        REGULAR, REWARDED
+    }
+
     List<Hotel> hotelList = new ArrayList<>();
 
     boolean addHotel(Hotel hotel) {
@@ -12,15 +17,17 @@ public class HotelReservationService {
         return true;
     }
 
-    Hotel getCheapestBestRatedHotel(String checkInDate, String checkOutDate,boolean isRewardedCustomer) {
-        if (isRewardedCustomer) {
-            hotelList.forEach(hotel ->{
+    Hotel getCheapestBestRatedHotel(String checkInDate, String checkOutDate, CustomerType customerType) throws DateFormatException {
+        validateInputDate(checkInDate);
+        validateInputDate(checkOutDate);
+        if (customerType.equals(CustomerType.REWARDED)) {
+            hotelList.forEach(hotel -> {
                 hotel.setWeekdayRate(hotel.getWeekdayRewardedRate());
                 hotel.setWeekendRate(hotel.getWeekendRewardedRate());
-            } );
+            });
         }
-        LocalDate inDate = LocalDate.of(Integer.valueOf(checkInDate.substring(6, 10)), Integer.valueOf(checkInDate.substring(3, 5)), Integer.valueOf(checkInDate.substring(0, 2)));
-        LocalDate outDate = LocalDate.of(Integer.valueOf(checkOutDate.substring(6, 10)), Integer.valueOf(checkOutDate.substring(3, 5)), Integer.valueOf(checkOutDate.substring(0, 2)));
+        LocalDate inDate = LocalDate.of(Integer.parseInt(checkInDate.substring(6, 10)), Integer.parseInt(checkInDate.substring(3, 5)), Integer.parseInt(checkInDate.substring(0, 2)));
+        LocalDate outDate = LocalDate.of(Integer.parseInt(checkOutDate.substring(6, 10)), Integer.parseInt(checkOutDate.substring(3, 5)), Integer.parseInt(checkOutDate.substring(0, 2)));
 
         long totalNumberOfDays = getNumberOfDays(inDate, outDate);
         long weekendDays = getNumberOfWeekendDays(inDate, outDate);
@@ -28,13 +35,23 @@ public class HotelReservationService {
 
         calculateTotalCost(weekDays, weekendDays);
         List<Hotel> hotelList1 = hotelList.stream().sorted((x, y) -> Long.compare(x.getTotalCost(), y.getTotalCost())).collect(Collectors.toList());
-        List<Hotel> hotelList2 = hotelList1.stream().filter(x -> x.getTotalCost() == hotelList1.get(0).getTotalCost()).sorted((x, y) -> Integer.compare(y.getRatings(), x.getRatings())).collect(Collectors.toList());
+        List<Hotel> hotelList2 = hotelList1.stream().filter(x -> x.getTotalCost() == hotelList1.get(0).getTotalCost()).sorted((x, y) -> Integer.compare(y.getRating(), x.getRating())).collect(Collectors.toList());
 
         return hotelList2.get(0);
     }
 
+    boolean validateInputDate(String date) throws DateFormatException {
+        Pattern pattern = Pattern.compile("\\d{2}[/-]{1}\\d{2}[/-]{1}\\d{4}");
+        boolean isValid = pattern.matcher(date).matches();
+        if (isValid) {
+            return true;
+        } else {
+            throw new DateFormatException("input date format is not correct");
+        }
+    }
+
     Hotel getBestRatedHotel() {
-        return hotelList.stream().sorted((x, y) -> Integer.compare(y.getRatings(), x.getRatings())).collect(Collectors.toList()).get(0);
+        return hotelList.stream().sorted((x, y) -> Integer.compare(y.getRating(), x.getRating())).collect(Collectors.toList()).get(0);
     }
 
     long getNumberOfDays(LocalDate checkInDate, LocalDate checkOutDate) {
@@ -44,15 +61,12 @@ public class HotelReservationService {
     long getNumberOfWeekendDays(LocalDate checkInDate, LocalDate checkOutDate) {
         Stream<LocalDate> weekendDays = checkInDate.datesUntil(checkOutDate).filter(date -> {
             DayOfWeek day = date.getDayOfWeek();
-            boolean isWeekendDay = (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY);
-            return isWeekendDay;
+            return (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY);
         });
-        return weekendDays.collect(Collectors.toList()).size();
+        return weekendDays.count();
     }
 
     void calculateTotalCost(long weekDays, long weekendDays) {
-        hotelList.stream().forEach(x -> {
-            x.setTotalCost(weekDays * x.getWeekdayRate() + weekendDays * x.getWeekendRate());
-        });
+        hotelList.forEach(x -> x.setTotalCost(weekDays * x.getWeekdayRate() + weekendDays * x.getWeekendRate()));
     }
 }
